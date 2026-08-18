e51b2b377ffda9faacb83cfc45a51ff96369cdb6
GUSTAVO HENRIQUE SANTOS DA SILVA <26171882@edu119.local>
2026-08-18 09:40:39 -0300

    Atualizando

---
+++ 01-Funcoes/02-Parametros.js
@@ -0,0 +1,23 @@
+function apresentar(nome)
+{
+console.log(`Olá, ${nome}`)
+}
+
+apresentar("Vitor");
+
+function somar(a, b)
+{
+
+console.log(`${a} + ${b} = ${a+b}`);
+
+}
+
+somar(8, 15);
+
+function apresentar(nome ="visitante")
+{
+    console.log(`Nome: ${nome}`);
+}
+
+apresentar()
+apresentar("Demi")
